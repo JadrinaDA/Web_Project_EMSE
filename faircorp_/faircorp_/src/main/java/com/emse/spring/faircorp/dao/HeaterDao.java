cf472e9916e3ee37b3d96201a46f6b9eb26e53ba
JadrinaDA <jadrinadeandrade@uc.cl>
2022-09-20 12:21:32 +0200

    Daos implemented

---
+++ faircorp_/faircorp_/src/main/java/com/emse/spring/faircorp/dao/HeaterDao.java
@@ -0,0 +1,12 @@
+package com.emse.spring.faircorp.dao;
+
+import com.emse.spring.faircorp.model.Heater;
+import com.emse.spring.faircorp.model.Window;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface HeaterDao extends JpaRepository<Heater, Long> {
+    @Query("select h from Heater h where h.id=:id")
+    Heater findbyId(@Param("id") Long id);
+}
